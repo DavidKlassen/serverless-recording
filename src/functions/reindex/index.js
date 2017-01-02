d16c7e6154e746d0a22d918e005211aa263876fc
David Klassen <f0rk.tt@gmail.com>
2017-01-02 15:32:57 +0100

    Added admin page

--- src/functions/reindex/index.js
+++ src/functions/reindex/index.js
@@ -14,25 +14,28 @@ const s3 = new AWS.S3();
 
 
 export default λ(async (e) => {
-    // download video file
-    const video = await s3.getObject({
-        Bucket: e.bucket,
-        Key: e.key
-    }).promise();
+    return Promise.all(e.Records.map(async (record) => {
+        // download video file
+        const video = await s3.getObject({
+            Bucket: record.s3.bucket.name,
+            Key: record.s3.object.key
+        }).promise();
 
-    // write file to /tmp
-    const file = tmp.fileSync({ postfix: '.webm' });
-    fs.appendFileSync(file.name, video.Body);
+        // write file to /tmp
+        const file = tmp.fileSync({ postfix: '.webm' });
+        fs.appendFileSync(file.name, video.Body);
 
-    // reindex
-    const dst = tmp.tmpNameSync({ postfix: '.webm' });
-    await ffmpeg(file.name, dst, '-y -c copy');
+        // reindex
+        const dst = tmp.tmpNameSync({ postfix: '.webm' });
+        await ffmpeg(file.name, dst, '-y -c copy');
 
-    // upload and return
-    return s3.upload({
-        Bucket: e.bucket,
-        Key: `${e.key}.reindexed.webm`,
-        Body: fs.readFileSync(dst),
-        ContentType: 'video/webm'
-    }).promise();
+        // upload and return
+        return s3.upload({
+            Bucket: record.s3.bucket.name,
+            Key: `${record.s3.object.key}.reindexed.webm`,
+            Body: fs.readFileSync(dst),
+            ContentType: 'video/webm',
+            ACL: 'public-read'
+        }).promise();
+    }));
 });
--- src/functions/thumb/index.js
+++ src/functions/thumb/index.js
@@ -14,27 +14,30 @@ const s3 = new AWS.S3();
 
 
 export default λ(async (e) => {
-    // download video file
-    const video = await s3.getObject({
-        Bucket: e.bucket,
-        Key: e.key
-    }).promise();
+    return Promise.all(e.Records.map(async (record) => {
+        // download video file
+        const video = await s3.getObject({
+            Bucket: record.s3.bucket.name,
+            Key: record.s3.object.key
+        }).promise();
 
-    // write file to /tmp
-    const file = tmp.fileSync({ postfix: '.webm' });
-    fs.appendFileSync(file.name, video.Body);
+        // write file to /tmp
+        const file = tmp.fileSync({ postfix: '.webm' });
+        fs.appendFileSync(file.name, video.Body);
 
-    // create thumbnail
-    const thumbName = tmp.tmpNameSync({ postfix: '.png' });
-    const filters = 'thumbnail,scale=200:150:force_original_aspect_ratio=increase,crop=200:150';
-    const args = `-vf "${filters}" -frames:v 1`;
-    await ffmpeg(file.name, thumbName, args);
+        // create thumbnail
+        const thumbName = tmp.tmpNameSync({ postfix: '.png' });
+        const filters = 'thumbnail,scale=200:150:force_original_aspect_ratio=increase,crop=200:150';
+        const args = `-vf "${filters}" -frames:v 1`;
+        await ffmpeg(file.name, thumbName, args);
 
-    // upload thumbnail and return
-    return s3.upload({
-        Bucket: e.bucket,
-        Key: `${e.key}.thumb.png`,
-        Body: fs.readFileSync(thumbName),
-        ContentType: 'image/png'
-    }).promise();
+        // upload thumbnail and return
+        return s3.upload({
+            Bucket: record.s3.bucket.name,
+            Key: `${record.s3.object.key}.thumb.png`,
+            Body: fs.readFileSync(thumbName),
+            ContentType: 'image/png',
+            ACL: 'public-read'
+        }).promise();
+    }));
 });
