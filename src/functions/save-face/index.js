fd8ddca00f175b4e379a3c8005ee9fc3a6c88e2a
David Klassen <f0rk.tt@gmail.com>
2017-01-06 15:34:10 +0100

    Cleanup

--- src/functions/save-face/index.js
+++ src/functions/save-face/index.js
@@ -10,16 +10,15 @@ import AWS from 'aws-sdk';
 const rekognition = new AWS.Rekognition();
 
 
-export default λ(async (e) => {
-    return Promise.all(e.Records.map(async (record) => rekognition.indexFaces({
-            CollectionId: 'media-recording-participants',
-            ExternalImageId: record.s3.object.key.split('/')[0],
-            Image: {
-                S3Object: {
-                    Bucket: record.s3.bucket.name,
-                    Name: record.s3.object.key
-                }
+export default λ(async(e) => {
+    return Promise.all(e.Records.map(async(record) => rekognition.indexFaces({
+        CollectionId: 'media-recording-participants',
+        ExternalImageId: record.s3.object.key.split('/')[0],
+        Image: {
+            S3Object: {
+                Bucket: record.s3.bucket.name,
+                Name: record.s3.object.key
             }
-        }).promise()
-    ));
+        }
+    }).promise()));
 });
